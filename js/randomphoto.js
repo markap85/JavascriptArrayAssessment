11d11d9aec47033d5ac07ee6a6f04400a6971b8e
Mark Peters <markalexanderpeters@gmail.com>
2025-07-24 11:20:16 +0100

    Created functionality to retrieve random image

--- js/randomphoto.js
+++ js/randomphoto.js
@@ -4,8 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const randomBtn = document.getElementById('button-random');
   if (randomBtn) {
     randomBtn.addEventListener('click', function() {
-      // TODO: Add logic to fetch and display a new random photo
-      console.log('Random Photo button clicked');
+      // Find the image element in the same container as the button
+      const container = randomBtn.closest('.container');
+      const img = container ? container.querySelector('img') : null;
+      if (img) {
+        // Add a random query param to force a new image
+        img.src = 'https://picsum.photos/800/600?random=' + Date.now();
+      }
     });
   }
 });
